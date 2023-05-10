58212e495623b24c62908a303e4dada342f0a131
Aleksey Kovalenko <kalexhaym@gmail.com>
2023-05-10 16:31:14 +0400

    Make open popunder by click and keydown events

--- src/AdopPop.js
+++ src/AdopPop.js
@@ -1,15 +1,45 @@
 export default class AdopPop {
   constructor(params) {
     this.zone = params.zone || null
+    this.popUnderOpened = false
 
     this.redirect()
   }
 
   redirect() {
+    let document = window.document
+
     const url = `https://pop.tier4apps.com/redirect-zone/${this.zone}`
-    setTimeout(function () {
-      window.onbeforeunload = null
-      window.open(url, '_blank')
-    }, 500)
+
+    const events = [
+      "click", "mousedown", "mouseup", "keydown", "keyup", "keypress"
+    ];
+
+    let isWindowNotClosed = function(w) {
+      return !(w ? w.closed : 1)
+    }
+
+    let listener = function (event) {
+      setTimeout(function () {
+        if (this.popUnderOpened) {
+          return
+        }
+
+        window.onbeforeunload = null
+
+        let blank = window.open('about:blank', '_blank')
+
+        if (isWindowNotClosed(blank)) {
+          this.popUnderOpened = true
+          blank.location.href = url
+        }
+      }, 50)
+    }
+
+    events.forEach(function(e) {
+      document.addEventListener(e, listener, {
+        once : true
+      })
+    });
   }
 }
